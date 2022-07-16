e9d95dfe35dde7c65357ccc6968c804c94d889b5
thsistemas <thiago@thsistemas.com.br>
2022-07-15 22:30:48 -0300

    Data Componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-
   return(
-    <h1>Gloria a Deus!!!</h1>
+    <>
+    <Header/>
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+          </div>
+          </section>
+          </main>
+    </>
   )
 
 }
